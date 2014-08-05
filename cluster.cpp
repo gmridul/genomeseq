e29aa0bb206a085953aeab1cadafe408cee419ea
soumitrakp <soumitrakp@gmail.com>
2014-08-05 14:59:52 +0530

    added function for binning the reads

--- cluster.cpp
+++ cluster.cpp
@@ -3,23 +3,31 @@
  * g++ -I/home/mridul/seqan/seqan-trunk/core/include -I/home/mridul/seqan/seqan-trunk/extras/include/ -lrt -W -Wall -Wno-long-long -pedantic -Wno-variadic-macros -std=c++0x match_reads.cpp
  */
 
-
 #include <iostream>
 #include <string>
 #include <cstdint>
 #include <fstream>
 #include <unordered_map>
+#include <vector>
+
 #include <seqan/align.h>
 #include <seqan/basic.h>
 #include <seqan/sequence.h>
 #include <seqan/file.h>
+using namespace seqan;
+
+#include <boost/pending/disjoint_sets.hpp>
+#include <boost/unordered/unordered_set.hpp>
+
+
 #define BOUND 5
 #define THRESHOLD 10
 using namespace std;
-using namespace seqan;
+
 
 typedef String<char> TSequence;                 // sequence type
 typedef Align<TSequence,ArrayGaps> TAlign;      // align type
+
 
 class llist {
     public:
@@ -32,6 +40,13 @@ class llist {
         side='c';
     }
 };
+
+#define RANK(x) (2*x->entrynum + (x->side == 'l' ? 0 : 1) + 1) // ranks are 1, 2, ...
+typedef unordered_map<int64_t,llist* > HashTable;
+
+typedef std::vector<int> VecInt;
+typedef boost::disjoint_sets<int*,int*> DisjointSets;
+
 
 bool match_reads(llist* x,llist* y) {//TSequence s1,TSequence s2,int k,int thold) {
     
@@ -48,16 +63,39 @@ bool match_reads(llist* x,llist* y) {//TSequence s1,TSequence s2,int k,int thold
     if(scores<=THRESHOLD) {
         return true;
     }
+
     return false;
 }
 
+
+void bin_reads(const HashTable &hashtab, DisjointSets &ds) {
+
+    for ( auto it = hashtab.begin(); it != hashtab.end(); ++it ) {
+        cout << it->first << ":";
+        for (auto x = it->second; x != NULL; x = x->next) {
+          int rank = RANK(x);
+          if (!ds.find_set(rank)) { // find_set returns 0 if not found
+            ds.make_set(rank); 
+          }
+        }
+        for (auto x = it->second; (x != NULL) && (x->next != NULL); x = x->next) {
+          int xrank = RANK(x);
+          for (auto y = x->next; y != NULL; y = y->next) {
+            int yrank = RANK(y);
+            if (match_reads(x, y)) {
+              ds.union_set(xrank, yrank);
+            }
+          }
+        }
+    }
+}
 
 
 int main(int argc, char*  argv[]) {
     int k,skipN=0,num=1;
     cin >> k; //k-mer size // not greater than 32
     int64_t slid,mask=(1<<(2*k))-1;
-    unordered_map<int64_t,llist* > hashtab;
+    HashTable hashtab;
     ifstream f1(argv[1]),f2(argv[2]);
     string p1,p2;
     int count=0, len;
@@ -183,10 +221,12 @@ int main(int argc, char*  argv[]) {
             temp=temp->next;
         }
     }
-    
-    
 
-
+    
+    VecInt rank (10);
+    VecInt parent (10);
+    DisjointSets ds(&rank[0], &parent[0]);
+    
 
     return 0;
 }
