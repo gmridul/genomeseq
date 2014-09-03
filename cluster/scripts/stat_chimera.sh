af67a101222783c39b9de768351632437fb40218
gmridul <gmridul09@gmail.com>
2014-09-03 16:12:55 +0530

    generated statistics for chimeras

--- cluster/scripts/stat_chimera.sh
+++ cluster/scripts/stat_chimera.sh
@@ -1,11 +1,17 @@
-../bowtie2-2.2.3/bowtie2-build ../genomeseq/oldinput/$1 $1
+#!/bin/bash
+
+../bowtie2-2.2.3/bowtie2-build ../genomeseq/input/$1 $1
 
 ../bowtie2-2.2.3/bowtie2 -x $1 -1 ../database/BAC-A-secondDatabaseFirstRE/$1*-1.fq -2 ../database/BAC-A-secondDatabaseFirstRE/$1*-2.fq --un-conc $1.unseq > $1.sam
 
 ../bowtie2-2.2.3/bowtie2 --local -x $1 -1 $1.1.unseq -2 $1.2.unseq > $1.local_unmapped.sam
 
-python ../genomeseq/cluster/cutSlocal.py $1.local_unmapped.sam > $1.localScut.fq
+python ../genomeseq/cluster/scripts/cutSlocal.py $1.local_unmapped.sam > $1.localScut.fq
 
 ../bowtie2-2.2.3/bowtie2 --local -x $1 -U $1.localScut.fq > $1.detect_chimera.sam
 
-awk '{print $3}' < detect_chimera.sam | grep -c 'gi'
+chim=$(awk '{print $3}' < $1.detect_chimera.sam | grep -c $1)
+echo $chim > chimera_$1
+num_line=$(wc -l $1.detect_chimera.sam | cut -d" " -f1)
+echo $num_line-$chim | bc >> chimera_$1
+rm $1.*
--- scripts/getbac.sh
+++ scripts/getbac.sh
@@ -1,12 +1,12 @@
 cd ../input
-for i in AC217266
+for i in AC185603 AC186236 AC189789 AC194009 AC205693 AC205879 AC206167 AC208225 AC209175 AC213049 AC214831 AC217266
 do
     wget http://www.ncbi.nlm.nih.gov/nuccore/$i 
     grep ReportShortCut15 $i > line$i 
     rm $i
     echo line$i
-    echo http://www.ncbi.nlm.nih.gov$(awk '{print $3}' line$i | awk 'BEGIN { FS="\"" } {print $2}' | head -1)\&format=text
-    ../scripts/casperjs crawl.js http://www.ncbi.nlm.nih.gov$(awk '{print $3}' line$i | awk 'BEGIN { FS="\"" } {print $2}' | head -1)\&format=text > $i
+    echo http://www.ncbi.nlm.nih.gov$(cut -d"\"" -f4 line$i)\&format=text
+    casperjs ../scripts/crawl.js http://www.ncbi.nlm.nih.gov$(cut -d"\"" -f4 line$i)\&format=text > $i
     rm line$i
-    sed -i -e 1,1d $i
+    #sed -i -e 1,1d $i
 done
