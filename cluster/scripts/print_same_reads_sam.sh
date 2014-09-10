4f3a12172ab8f6de422cebd394a66458fd126c21
gmridul <gmridul09@gmail.com>
2014-09-10 17:51:18 +0530

    modified the script

--- cluster/scripts/print_same_reads_sam.sh
+++ cluster/scripts/print_same_reads_sam.sh
@@ -1,9 +1,14 @@
 #copy the script on command line and replace 68443 with the required number ( it is the position at which the reads starts aligning to the reference genome"
 awk  '{
-if ($4 == 36844 && and($2,4)==0) {
+if ($4 == 3854 && and($2,4)==0) {
     printf "@"; print $1; 
+    #print "##########################################################"
+    #print $2;
+    #print $10;
+    #print "##########################################################"
     if(and($2,16)==16) {
         len = length($10)
+        print "**********************************************"
         for(i=len;i!=0;i--) {
             if(substr($10,i,1)=="'"A"'") printf "'"T"'"
             else if(substr($10,i,1)=="'"T"'") printf "'"A"'"
@@ -11,7 +16,6 @@ if ($4 == 36844 && and($2,4)==0) {
             else if(substr($10,i,1)=="'"G"'") printf "'"C"'"
         }
         print "\n+"; 
-        x=""
         for(i=len;i!=0;i--) {
             printf substr($11,i,1)
         }
